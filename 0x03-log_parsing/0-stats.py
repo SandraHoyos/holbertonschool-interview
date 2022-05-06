95665fff31c19c294f3a868786ff3ff233bb7797
Sandra Hoyos <slhoyosr@gmail.com>
2022-05-05 22:55:40 -0500

    fix pycode

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,39 @@
 #!/usr/bin/python3
+"""stats module
 """
-Python script takes URL from stdin and compute exact metrics
-"""
+from sys import stdin
 
 
-def printst(dictionary, size):
-    """
-    Print function
-    """
-    d = sorted(dictionary.keys())
-    print("File size: {:d}".format(size))
-    for i in d:
-        if dictionary[i] != 0:
-            print("{}: {:d}".format(i, dictionary[i]))
-
-
-count = 0
+codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 size = 0
-dic = {"200": 0, "301": 0, "400": 0, "401": 0,
-       "403": 0, "404": 0, "405": 0, "500": 0}
 
-try:
-    with open(0) as f:
-        for line in f:
-            count += 1
-            arr = line.split()
-            try:
-                size += int(arr[-1])
-            except:
-                pass
-            try:
-                st = arr[-2]
-                if st in dic:
-                    dic[st] += 1
-            except:
-                pass
-            if count % 10 == 0:
-                printst(dic, size)
-        printst(dic, size)
 
-except KeyboardInterrupt:
-    printst(dic, size)
-    raise
+def print_info():
+    """print_info method print needed info
+    Args:
+        codes (dict): code status
+        size (int): size of files
+    """
+    print("File size: {}".format(size))
+    for key, val in sorted(codes.items()):
+        if val > 0:
+            print("{}: {}".format(key, val))
+
+
+if __name__ == '__main__':
+    try:
+        for i, line in enumerate(stdin, 1):
+            try:
+                info = line.split()
+                size += int(info[-1])
+                if info[-2] in codes.keys():
+                    codes[info[-2]] += 1
+            except Exception:
+                pass
+            if not i % 10:
+                print_info()
+    except KeyboardInterrupt:
+        print_info()
+        raise
+    print_info()
