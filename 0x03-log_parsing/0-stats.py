45006a98644cde3204d54734df444db787b8f7de
Sandra Hoyos <slhoyosr@gmail.com>
2022-05-08 23:20:40 -0500

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,41 @@
 #!/usr/bin/python3
-"""Write a script that reads stdin line by line and computes metrics:
+"""
+reads stdin line by line and computes metrics
 """
 
-if __name__ == '__main__':
-    import sys
+import sys
 
-    def statistics(status_counts_dict, total_file_size):
-        """ prints statistics at that moment"""
-        print("File size: {:d}".format(total_file_size))
-        for code in sorted(status_counts_dict.keys()):
-            value = status_counts_dict[code]
-            if value != 0:
-                print("{}: {}".format(code, value))
+STATUS = {'200': 0,
+          '301': 0,
+          '400': 0,
+          '401': 0,
+          '403': 0,
+          '404': 0,
+          '405': 0,
+          '500': 0}
 
-    # initialize variables
-    status_codes = {"200": 0, "301": 0, "400": 0,
-                    "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-    total_file_size = 0
-    count = 0
-    try:
-        # catch input line
-        for line in sys.stdin:
-            # count and split line
-            line = line.split()
-            count += 1
-
-            try:
-                status_code, file_size = line[-2], int(line[-1])
-                total_file_size += file_size
-
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-
-            except Exception:
-                pass
-
-            if count % 10 == 0:
-                statistics(status_codes, total_file_size)
-
-        statistics(status_codes, total_file_size)
-
-    except (KeyboardInterrupt, SystemExit):
-        statistics(status_codes, total_file_size)
-        raise
+total_size = 0
+n = 0
+try:
+    for argument in sys.stdin:
+        arguments = argument.split(" ")
+        if len(arguments) > 2:
+            status = arguments[-2]
+            file_size = int(arguments[-1])
+            if status in STATUS:
+                STATUS[status] += 1
+            total_size += file_size
+            n += 1
+            if n == 10:
+                print("File size: {:d}".format(total_size))
+                for key, value in sorted(STATUS.items()):
+                    if value != 0:
+                        print("{}: {:d}".format(key, value))
+                n = 0
+except KeyboardInterrupt:
+    pass
+finally:
+    print("File size: {:d}".format(total_size))
+    for key, value in sorted(STATUS.items()):
+        if value != 0:
+            print("{}: {:d}".format(key, value))
