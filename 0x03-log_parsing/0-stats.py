788f9aeba2509ee8629c58a98819954c1c4a353d
Sandra Hoyos <slhoyosr@gmail.com>
2022-05-12 21:17:41 -0500

    fix blank line

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python3
 """
-reads stdin line by line and computes metrics
+Script that reads stdin line by line and computes metrics:
 """
 
 import sys
 
-STATUS = {'200': 0,
-          '301': 0,
-          '400': 0,
-          '401': 0,
-          '403': 0,
-          '404': 0,
-          '405': 0,
-          '500': 0}
 
-total_size = 0
-n = 0
-try:
-    for argument in sys.stdin:
-        arguments = argument.split(" ")
-        if len(arguments) > 2:
-            status = arguments[-2]
-            file_size = int(arguments[-1])
-            if status in STATUS:
-                STATUS[status] += 1
-            total_size += file_size
-            n += 1
-            if n == 10:
-                print("File size: {:d}".format(total_size))
-                for key, value in sorted(STATUS.items()):
-                    if value != 0:
-                        print("{}: {:d}".format(key, value))
-                n = 0
-except KeyboardInterrupt:
-    pass
-finally:
-    print("File size: {:d}".format(total_size))
-    for key, value in sorted(STATUS.items()):
-        if value != 0:
-            print("{}: {:d}".format(key, value))
+if __name__ == "__main__":
+
+    status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+                   "403": 0, "404": 0, "405": 0, "500": 0}
+    file_size = 0
+    total_lines = 0
+
+    def print_values(status_code, file_size):
+        print("File size: {:d}".format(file_size))
+        for key in sorted(status_code.keys()):
+            if status_code[key] != 0:
+                print("{}: {:d}".format(key, status_code[key]))
+
+    try:
+        for line in sys.stdin:
+            if total_lines != 0 and total_lines % 10 == 0:
+                print_values(status_code, file_size)
+
+            total_lines += 1
+            ln = line.split()
+
+            try:
+                file_size += int(ln[-1])
+            except:
+                pass
+
+            try:
+                if ln[-2] in status_code:
+                    status_code[ln[-2]] += 1
+            except:
+                pass
+
+        print_values(status_code, file_size)
+
+    except KeyboardInterrupt:
+        print_values(status_code, file_size)
+        raise
+    
