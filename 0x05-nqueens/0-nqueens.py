80e66f479306f30e702a22d68c00fa3ae4c15c54
Sandra Hoyos <slhoyosr@gmail.com>
2022-05-21 17:39:30 -0500

    fix 2

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,54 +1,61 @@
 #!/usr/bin/python3
+""" N queens backtracking """
+import sys
 
 
-"""program that solves the N queens problem."""
-from sys import argv, exit
+class NQueen:
+    """ Class Queens """
+
+    def __init__(self, n):
+        """ Constructor """
+        self.n = n
+        self.x = [0 for i in range(n + 1)]
+        self.res = []
+
+    def place(self, k, i):
+        """ Check if a secure place
+        """
+
+        for j in range(1, k):
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        """ Resolve the nqueen
+        """
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                self.x[k] = i
+                if k == self.n:
+                    solution = []
+                    for i in range(1, self.n + 1):
+                        solution.append([i - 1, self.x[i] - 1])
+                    self.res.append(solution)
+                else:
+                    self.nQueen(k + 1)
+        return self.res
 
 
-if len(argv) != 2:
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
-N = argv[1]
+N = sys.argv[1]
 
 try:
     N = int(N)
 except ValueError:
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 
 if N < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
-solution = []
+queen = NQueen(N)
+result = queen.nQueen(1)
 
-
-def nqueens(row, N, solution):
-    """The program should print any possible solution"""
-    if (row == N):
-        print(solution)
-    else:
-        for col in range(N):
-            position = [row, col]
-            if validposition(solution, position):
-                solution.append(position)
-                nqueens(row + 1, N, solution)
-                solution.remove(position)
-
-
-def validposition(solution, position):
-    """validate horizontal and diagonal position of queens"""
-    for queen in solution:
-        if queen[1] == position[1]:
-            return False
-        # descending diagonal
-        if (queen[0] - queen[1]) == (position[0] - position[1]):
-            return False
-        # ascending diagonal
-        if (queen[0] + queen[1]) == (position[0] + position[1]):
-            return False
-    return True
-
-
-nqueens(0, N, solution)
+for i in result:
+    print(i)
