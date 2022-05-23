b5bc5c6b3baf4ef1d9b266c1a38f91bed489a755
Sandra Hoyos <slhoyosr@gmail.com>
2022-05-22 20:50:42 -0500

    fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -46,16 +46,3 @@ N = sys.argv[1]
 
 try:
     N = int(N)
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-queen = NQueen(N)
-result = queen.nQueen(1)
-
-for i in result:
-    print(i)
