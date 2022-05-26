e13f249483742dcac7f755f3eb7657ac48d2e6c9
Sandra Hoyos <slhoyosr@gmail.com>
2022-05-25 21:05:21 -0500

    Star War API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* prints all characters of star wars */
+
+const request = require('request');
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async (err, res, body) => {
+  err && console.log(err);
+
+  const arrayPersonajes = (JSON.parse(res.body).characters);
+
+  for (const personaje of arrayPersonajes) {
+    await new Promise((resolve, reject) => {
+      request(personaje, (err, res, body) => {
+        err && console.log(err);
+        console.log(JSON.parse(body).name);
+        resolve();
+      }
+      );
+    });
+  }
+});
