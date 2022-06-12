f798ad9b96af04ffc9a44c59db583bbbbb6614ac
Sandra Hoyos <slhoyosr@gmail.com>
2022-06-12 14:42:15 -0500

    new

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
 """
-Making Change Function with the few amount of coins
+Determine the fewest number of coins needed to meet a given amount total.
 """
 
 
-def makeChange(coins: list, total: int) -> int:
+def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
-    Returns: fewest number of coins needed to meet total
-        - If total is 0 or < 0, return 0
-        - If total cannot be met by any number of coins you have, return -1
-        - Coins is a list of the values of the coins
-        - The value of a coin will always be an integer greater than 0
-        - You can assume you have an infinite number of each denomination
-        of coin in the list
+    Args:
+    coins ([list]): a list of the values of the coins in your possession
+    total ([number]): amount
+    Return: fewest number of coins needed to meet total
     """
     if total <= 0:
         return 0
 
-    change = 0
     coins.sort(reverse=True)
 
-    for coin in coins:
-        temp_change = int(total / coin)
-        total -= temp_change * coin
-        change += temp_change
-        if total == 0:
-            return change
-    if total != 0:
-        return -1
+    i, n_coins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
+
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            n_coins += 1
+        else:
+            i += 1
+
+    check = cpy_total > 0 and n_coins > 0
+    return -1 if check or n_coins == 0 else n_coins
