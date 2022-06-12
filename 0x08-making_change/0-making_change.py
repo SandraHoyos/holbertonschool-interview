35075998a927d3cb5989e12c14325e6529c76fe4
Sandra Hoyos <slhoyosr@gmail.com>
2022-06-12 09:52:28 -0500

    pep8 fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
-"""Making change O(n)"""
+"""
+Program to determin the fewest number of coins
+needed to meet a given amount total
+"""
 
 
 def makeChange(coins, total):
-    """Clasic Bottom-Up dynamic programming"""
-    temp_value = 0
-    coins.sort(reverse=True)
-
-    if total < 0:
+    """
+    Given a pile of coins of different values, determine
+    the fewest number of coins needed to meet a given amount total.
+    Return: least number of coins necessary to meet total.
+    - If total is 0 or less, return 0
+    - If total cannot be met by any number of coins you have, return -1
+    - value of a coin will always be an integer greater than 0
+    - coins is a list of the values of the coins in your possession
+    """
+    if total <= 0:
         return 0
 
-    for coin in coins:
-        if total % coin <= total:
-            temp_value += total // coin
-            total = total % coin
+    valNew = total + 1
+    store = {0: 0}
 
-    return temp_value if total == 0 else -1
+    for i in range(1, total + 1):
+        store[i] = valNew
+
+        for coin in coins:
+            immediate = i - coin
+            if immediate < 0:
+                continue
+
+            store[i] = min(store[immediate] + 1, store[i])
+
+    if store[total] == total + 1:
+        return -1
+
+    return store[total]
