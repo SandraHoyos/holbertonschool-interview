d1c5da94dfd1609e606d19c36c99fc8a25be6c32
Sandra Hoyos <slhoyosr@gmail.com>
2022-06-12 09:57:18 -0500

    Efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python3
 """
-Program to determin the fewest number of coins
-needed to meet a given amount total
+Given a pile of coins of different values,
+determine the fewest number of coins needed
+to meet a given amount total.
 """
 
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine
-    the fewest number of coins needed to meet a given amount total.
-    Return: least number of coins necessary to meet total.
-    - If total is 0 or less, return 0
-    - If total cannot be met by any number of coins you have, return -1
-    - value of a coin will always be an integer greater than 0
-    - coins is a list of the values of the coins in your possession
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed
+    to meet a given amount total
     """
     if total <= 0:
         return 0
-
-    valNew = total + 1
-    store = {0: 0}
-
-    for i in range(1, total + 1):
-        store[i] = valNew
-
-        for coin in coins:
-            immediate = i - coin
-            if immediate < 0:
-                continue
-
-            store[i] = min(store[immediate] + 1, store[i])
-
-    if store[total] == total + 1:
+    if not coins or min(coins) > total:
         return -1
-
-    return store[total]
+    num_coins = 0
+    coins.sort(reverse=True)
+    i = 0
+    while i < len(coins):
+        if coins[i] <= total:
+            num_coins += total // coins[i]
+            total = total % coins[i]
+        i += 1
+    if total == 0:
+        return num_coins
+    else:
+        return -1
