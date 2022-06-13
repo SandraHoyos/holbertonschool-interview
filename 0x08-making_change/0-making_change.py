f153750e9dd15628390448289f9ca937c6ce82d6
Sandra Hoyos <slhoyosr@gmail.com>
2022-06-12 21:41:48 -0500

    nuevo

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,44 +1,32 @@
 #!/usr/bin/python3
 """
-Making Change
+Making Change Function with the few amount of coins
 """
 
 
-def makeChange(coins, total):
-"""
-Return the minimum number of coins needed to meet a given total
-Args:
-coins (list of ints): a list of coins of different values
-total (int): total value to be met
-Return:
-Number of coins or -1 if meeting the total is not possible
-"""
-  if total <= 0:
-      return 0
-  if coins == [] or coins is None:
-      return -1
-  try:
-      n = coins.index(total)
-      return 1
-  except ValueError:
-      pass
+def makeChange(coins: list, total: int) -> int:
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Returns: fewest number of coins needed to meet total
+        - If total is 0 or < 0, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination
+        of coin in the list
+    """
+    if total <= 0:
+        return 0
 
-  coins.sort(reverse=True)
-  coin_count = 0
-  for i in coins:
-      if total % i == 0:
-          coin_count += int(total / i)
-          return coin_count
-      if total - i >= 0:
-          if int(total / i) > 1:
-              coin_count += int(total / i)
-              total = total % i
-          else:
-              coin_count += 1
-              total -= i
-              if total == 0:
-                  break
-  if total > 0:
-      return -1
-  return coin_count
+    change = 0
+    coins.sort(reverse=True)
 
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= temp_change * coin
+        change += temp_change
+        if total == 0:
+            return change
+    if total != 0:
+        return -1
